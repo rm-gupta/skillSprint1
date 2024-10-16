5a5d3fae3e02d2f95eef7d3a1e696f2c2cd0dad5
Ritu Gupta <ritumarygupta@gmail.com>
2024-10-16 13:06:29 -0500

    Initial Commit

---
+++ skillSprint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skillSprint
+//
+//  Created by Ritu Gupta on 10/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

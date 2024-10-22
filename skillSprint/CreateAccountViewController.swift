d8c9d595ac82781a816bea55492548312703204e
divyanitin3 <divya.nitin66@gmail.com>
2024-10-22 14:33:40 -0500

    added login screen, create account screen, firebase data

---
+++ skillSprint/CreateAccountViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CreateAccountViewController.swift
+//  skillSprint
+//
+//  Created by Divya Nitin on 10/22/24.
+//
+
+import UIKit
+import FirebaseAuth
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func createAccountButtonPressed(_ sender: Any) {
+        guard let email = emailField.text, !email.isEmpty,
+              let password = passwordField.text, !password.isEmpty else {
+            errorMessage.text = "Please enter email and password."
+            return
+        }
+        
+        // Create new user in Firebase
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            if let error = error as NSError? {
+                self.errorMessage.text = "\(error.localizedDescription)"
+            } else {
+                self.errorMessage.text = ""
+                // Automatically log in the user after account creation
+                self.loginAfterAccountCreation(email: email, password: password)
+            }
+        }
+    }
+
+    func loginAfterAccountCreation(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            if let error = error as NSError? {
+                self.errorMessage.text = "\(error.localizedDescription)"
+            } else {
+                self.errorMessage.text = ""
+                self.performSegue(withIdentifier: "goToHomeScreenCreate", sender: self)
+            }
+        }
+    }
+}
+

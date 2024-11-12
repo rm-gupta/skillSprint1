8d35c622ac1baf495a547eea8cb70339f222fd58
Heyu Zhou <regina.zhou@utexas.edu>
2024-11-12 13:59:08 -0600

    Added Skills Library Screen with sort and filter features

---
+++ skillSprint/LibraryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LibraryTableViewCell.swift
+//  skillSprint
+//
+//  Created by Heyu Zhou on 11/11/24.
+//
+
+import UIKit
+
+class LibraryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ skillSprint/Skills.swift
@@ -0,0 +1,8 @@
+//
+//  Skills.swift
+//  skillSprint
+//
+//  Created by Heyu Zhou on 11/11/24.
+//
+
+import Foundation
